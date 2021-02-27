e5fcd0abed2d687b9b2b1df285f44102f10eb88e
unknown <vahid.sabetumz@gmail.com>
2021-02-27 17:00:05 +0330

    generate Question Model via artisan

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+
+use function GuzzleHttp\Psr7\str;
 
 class User extends Authenticatable
 {
@@ -40,4 +43,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
